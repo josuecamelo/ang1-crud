b9c2fc96f36e0bbac9b42418883dfae650493baa
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2017-12-19 13:31:29 -0200

    Adicionando Contato

---
+++ src/app/app.service.js
@@ -0,0 +1,23 @@
+/**
+ * Created by josuecamelo on 19/12/17.
+ */
+
+//paga guardar os dados
+
+angular
+    .module('app')
+    .service('ContactService', function() {
+        var list = [];
+
+        this.getList = function() {
+            return list;
+        };
+
+        this.setList = function(_list) {
+            list = _list;
+        };
+
+        this.push = function(contact) {
+            list.push(contact);
+        };
+    });
